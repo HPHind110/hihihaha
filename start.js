43b7bc53bb5cf3bc07d0665b904db3661867feb6
HPHind110 <huanpham.pdh@gmail.com>
2022-02-25 17:46:01 +0700

    Add

--- start.js
+++ start.js
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-console.log('Bot lao di thang loz!\nDit me thang nay lao nhi ?');
-=======
-console.log('Bot lao di thang loz! Heheh');
->>>>>>> 196ba8b86845dedeb848f2475a4d3d3166ab615f
+console.log('Goodbye!');
+
